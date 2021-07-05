08f9339fe3633a838bae6421638bdfd16be2d25a
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-07-05 17:16:18 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,7 +1,7 @@
 // Add your custom JavaScript for storefront pages here.
 setTimeout(function(){ 
-    if($('#container-home').length) {
-    $('#container-home > .container').append($('#container-home').nextAll())
+    if($('.container-home').length) {
+    $('.container-home > .container').append($('.container-home').nextAll())
 } 
 }, 500);
  
